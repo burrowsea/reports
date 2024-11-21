f86a1108344f2a18906c1b0bf3b784f1a9f0e096
burrows.ea <burrows.ea@oundleschool.org.uk>
2024-11-21 10:23:48 +0000

    bosh

--- connections.php
+++ connections.php
@@ -6,5 +6,5 @@ $dbname = "schoolreports";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully"; 
+echo("Connected successfully"); 
 ?>
