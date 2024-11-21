016af4d071ccd24f976148d3bb94a7bc9fefd3f9
burrows.ea <burrows.ea@oundleschool.org.uk>
2024-11-21 10:40:15 +0000

    endex

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "Options";
+$dbname = "schoolreports";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
--- install.php
+++ install.php
@@ -12,4 +12,31 @@ year INT(2) NOT NULL,
 role TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
+echo("tblusers created")
+$stmt = $conn->prepare("DROP TABLE IF EXISTS tblsubjects;
+CREATE TABLE tblsubjects
+(subjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+subjectname VARCHAR(20) NOT NULL,
+teacher VARCHAR(20) NOT NULL);");
+$stmt->execute();
+$stmt->closeCursor();
+echo("tblusers created")
 ?>
+
+
+
+
+
+CREATE TABLE TblSubjects(SubjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Subjectname VARCHAR(20) NOT NULL,
+Teacher VARCHAR(20) NOT NULL);
+
+
+
+CREATE TABLE TblPupilStudies(Subjectid INT(4),
+Userid INT(4),
+Classposition INT(2),
+Classgrade  CHAR(1),
+Exammark INT(2),
+Comment TEXT,
+PRIMARY KEY(Subjectid,Userid));
