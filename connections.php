dbf3d2dc9972f8fa5c5289874aca42d754a7b7b7
burrows.ea <burrows.ea@oundleschool.org.uk>
2024-11-21 10:22:44 +0000

    c

--- connections.php
+++ connections.php
@@ -4,13 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "schoolreports";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Connected successfully"; 
 ?>
