68e55b76a531e3ea345e07fbdedf402b5e5bb47f
burrows.ea <burrows.ea@oundleschool.org.uk>
2024-11-28 10:04:43 +0000

    save

--- install.php
+++ install.php
@@ -1,42 +1,15 @@
 <?php
 include_once("connection.php");
-$stmt = $conn->prepare("DROP TABLE IF EXISTS tblusers;
-CREATE TABLE tblusers 
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblUser;
+CREATE TABLE TblUsers 
 (UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-gender VARCHAR(1) NOT NULL,
-surname VARCHAR(20) NOT NULL,
-forename VARCHAR(20) NOT NULL,
-password VARCHAR(20) NOT NULL,
-house VARCHAR(20) NOT NULL,
-year INT(2) NOT NULL,
-role TINYINT(1))");
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
-echo("tblusers created")
-$stmt = $conn->prepare("DROP TABLE IF EXISTS tblsubjects;
-CREATE TABLE tblsubjects
-(subjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-subjectname VARCHAR(20) NOT NULL,
-teacher VARCHAR(20) NOT NULL);");
-$stmt->execute();
-$stmt->closeCursor();
-echo("tblusers created")
 ?>
-
-
-
-
-
-CREATE TABLE TblSubjects(SubjectID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Subjectname VARCHAR(20) NOT NULL,
-Teacher VARCHAR(20) NOT NULL);
-
-
-
-CREATE TABLE TblPupilStudies(Subjectid INT(4),
-Userid INT(4),
-Classposition INT(2),
-Classgrade  CHAR(1),
-Exammark INT(2),
-Comment TEXT,
-PRIMARY KEY(Subjectid,Userid));
